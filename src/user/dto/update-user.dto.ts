23912442a7d533a3f09c1654faca5d578f231e0f
dheyvid-lima-martinello <83586372+dheyvidlima@users.noreply.github.com>
2023-08-17 10:31:40 -0400

    fix: dto edit user

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,16 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @IsEmail()
+  @IsOptional()
+  email: string;
+  @IsOptional()
+  @IsString()
+  name?: string;
+  @IsString()
+  @IsOptional()
+  @MinLength(6)
+  senha: string;
+}
