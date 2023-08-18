334c87763e6a74732e112d3aaba14daa4b82d7bc
dheyvid-lima-martinello <83586372+dheyvidlima@users.noreply.github.com>
2023-08-18 16:32:12 -0400

    feat:  CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
